056a2284006a7c657dec2243aa3dfa8a3bec2bf6
Lanai Conceição <conceicao.lanai@gmail.com>
2021-09-20 13:57:18 -0300

    Requisito 4

--- src/components/Select.js
+++ src/components/Select.js
@@ -2,22 +2,38 @@ import React, { useState, useContext } from 'react';
 import MyContext from '../context/MyContext';
 
 function Select() {
+  const columns = [
+    'population',
+    'orbital_period',
+    'diameter',
+    'rotation_period',
+    'surface_water'];
+  // Trazer meu estado global
   const { data, setData } = useContext(MyContext);
+  // Criar meu estado local
   const [selected, setSelected] = useState({
     column: 'population',
     comparison: '>',
     value: 0,
   });
+  const [filterColumns, setFilterColumns] = useState(
+    columns,
+  );
 
-  // Função que cria as options de column
-  const columnGenerate = () => {
-    const columns = ['population',
-      'orbital_period', 'diameter', 'rotation_period', 'surface_water'];
-    const columnMap = columns.map((option, id) => <option key={ id }>{ option }</option>);
-    return columnMap;
+  // Função do filtro que não deve se repetir (requisito 4)
+  const noRepeat = () => {
+    setFilterColumns(columns);
+    const filtered = filterColumns.filter((item) => item !== selected.column);
+    setFilterColumns(filtered);
   };
 
-  // Função que cria as options de comparison
+  // Função que cria as <options> de column
+  const columnGenerate = () => {
+    const columnGenerated = filterColumns.map((option, id) => <option key={ id }>{ option }</option>);
+    return columnGenerated;
+  };
+
+  // Função que cria as <options> de comparison
   const comparisonGenerate = () => {
     const comparision = ['maior que',
       'menor que', 'igual a'];
@@ -26,7 +42,7 @@ function Select() {
     return comparisionMap;
   };
 
-  // Função executada no click do select:
+  // Função executada nos onChanges:
   const handleChange = (event) => {
     // mudando o estado do meu selected:
     setSelected({
@@ -39,29 +55,27 @@ function Select() {
     if (selected.comparison === 'maior que') {
       const biggerThan = data
         .filter((item) => Number(item[selected.column]) > Number(selected.value));
-      console.log(biggerThan);
       return biggerThan;
     }
     if (selected.comparison === 'menor que') {
       const lowerThan = data
         .filter((item) => Number(item[selected.column]) < Number(selected.value));
-      console.log(lowerThan);
       return lowerThan;
     }
     const equalTo = data
       .filter((item) => Number(item[selected.column]) === Number(selected.value));
-    console.log(equalTo);
     return equalTo;
   };
 
+  // Função usada no click do botão que envia para o estado global nossa condição
   const onChangeComparison = () => {
     setData(conditionComparison);
+    noRepeat();
   };
 
   return (
 
     <div>
-      console.log(data)
       <label htmlFor="name">
         <select
           id="column"
